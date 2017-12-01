8bb1aa8d3b5115a0b5eeff443e5eb58440eeb499
Russell-Greta <albertboehm@hotmail.de>
2017-12-01 12:04:43 +0700

    Predictions by my model match findings of researchers in original publication. Outputs Accuracy for different tolerance values as well as Mean Average Deviation when using test set for predictions.

--- linearRegressionFunctions.py
+++ linearRegressionFunctions.py
@@ -1,23 +1,56 @@
 import numpy as np
+import time
 
 def feature_scaling(X):
     m = X.shape[0]
-    means = np.sum(X, 0)/m
-    sd = np.sqrt(np.power(np.sum(X, 0),2)/(m-1))
-    X = np.divide(np.subtract(X, means), sd)
-    X[:, 0] = 1
+    means = np.mean(X, axis=0)
+    X = X-means
+    sd = np.std(X, axis=0)
+    X = X/sd
     return X
 
 def compute_cost_and_gradient(theta, X, Y):
     # m = number of training examples
     m = X.shape[0]
-
     #compute cost
     J = np.sum(np.power(np.dot(X, theta)-Y, 2))/(2*m)
     
     #compute gradient
-    gradients = (np.dot(X.T,np.power(np.dot(X, theta)-Y, 2)))/m
+    gradients = (np.dot((np.dot(X, theta)-Y).T,X).T)/m
     
     return J, gradients
+
+def perform_gradient_descent(theta, X, Y, alpha, iterations,logging = False):
+    for i in range(iterations+1):
+        start = time.time()
+        J, gradients = compute_cost_and_gradient(theta, X, Y)
+        theta = theta - alpha * gradients
+        if logging:
+            log_progress(i, iterations, start, J)
+    return J, theta
+
+
+
+def log_progress(current_iteration, iterations, start_time, J):
+    stop = time.time()
+    print('Estimated time remaining: ' + str((stop-start_time)*1000*(iterations-current_iteration)) + 'ms')
+    print('Current Cost: ' + str(J))
+    print('Performing gradient descent (step ' + str(current_iteration) + '/' + str(iterations) + ')\n')
     
-    
+def predict(theta, X, Y):
+    print ('- - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
+    m = X.shape[0]
+    predictions = np.dot(X,theta)
+    deviations = np.abs(predictions-Y)
+
+    tolerances = [0.25,0.5,1]
+    accuracies = [0,0,0]
+
+    for x in tolerances:
+        for i in range(0,m):
+            if deviations[i] <= x:
+                accuracies[tolerances.index(x)] += 1
+        print('Accuracy (tolerance = ' + str(x) + '): ' + format(float(accuracies[tolerances.index(x)])/m*100, '.2f') + '%')
+            
+    mad = np.mean(np.abs(predictions-Y))
+    print('Mean Average Deviation (MAD) on test set: ' + str(mad) + '\n')
--- model.py
+++ model.py
@@ -7,22 +7,34 @@ filename = 'winequality-red.csv'
 raw_file = open(filename, 'rt')
 raw_data = np.loadtxt(raw_file, delimiter=';')
 (number_examples, feature)= raw_data.shape
+
+#Shuffle Array to prevent bias in the training/test set
+np.random.shuffle(raw_data)
+
 data_train = raw_data[0:1119, 0:11]
 data_test = raw_data[1119:number_examples, 0:11]
 
 Y_train = raw_data[0:1119, 11:12]
 Y_test = raw_data[1119:number_examples, 11:12]
 
-#Extend both data_train/data_test with X0 = 1, respectively
-x0 = np.ones((data_train.shape[0], 1))
-data_train = np.append(x0, data_train, axis=1)
-data_test = np.append(x0, data_train, axis=1)
-
-theta = np.ones((data_train.shape[1], 1))
+#setting hyperparameters
+alpha = 0.1
+max_iterations = 1000
 
 #perform feature scaling
 data_train = functionality.feature_scaling(data_train)
+data_test = functionality.feature_scaling(data_test)
+
+#Extend both data_train/data_test with X0 = 1, respectively
+x0 = np.ones((data_train.shape[0], 1))
+data_train = np.append(x0, data_train, axis=1)
+x0 = np.ones((data_test.shape[0], 1))
+data_test = np.append(x0, data_test, axis=1)
+
+theta = np.zeros((data_train.shape[1], 1))
 
 #testing only
-J, gradients = functionality.compute_cost_and_gradient(theta, data_train, Y_train)
-print(J)
+log = True
+J, final_theta = functionality.perform_gradient_descent(theta, data_train, Y_train, alpha, max_iterations, log)
+
+functionality.predict(final_theta, data_test, Y_test)
